1fc3b522fa239928eb234effa43af5d192ea96da
Sharika Ahsan <sahsan5@binghamton.edu>
2022-09-18 21:44:00 -0400

    commit

--- hw1q1.c
+++ hw1q1.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 
 // Question 1:
-char* print_any_base(unsigned long num, unsigned int base) 
-{    
+char* print_any_base(unsigned long num, unsigned int base)
+{
     int a;
     int b;
     int count = 0;
     unsigned long number = num;
-    
-    /* 
-    casting: 
+
+    /*
+    casting:
     int *p = (int *) malloc (sizeof(int) * 10);
     */
     char *output = (char *) malloc (sizeof(char) * (count));
     if (num == 0) output[0] = '0';
-    
+
     /*
     The string must be terminated by ’\0’
     ‘\0’: terminating char of a string
@@ -24,7 +24,7 @@ char* print_any_base(unsigned long num, unsigned int base)
     return output;
 }
 
-int main() 
+int main()
 {
     return 0;
 }

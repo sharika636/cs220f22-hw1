4e319b879759a4e3c0d2feb4529cfed25e98b521
Sharika Ahsan <sahsan5@binghamton.edu>
2022-10-02 15:40:06 -0400

    move files into folders

---
+++ hw1q3/hw1q3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Question 3: accepts a string and reverses the word ordering within the string
+char *reverse_words(char *str)
+{
+    return ;
+}
+
+int main()
+{
+    return 0;    
+}
---
+++ hw1q5/hw1q5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Question 5: accepts a void pointer that points to an array of bytes 
+// in memory along with the number of bytes the pointer points to
+void paritize (void *p, unsigned int num_bytes)
+{
+    return ;
+}
+
+int main()
+{
+    return 0;    
+}
